3fcff04d6400e1a5770ae54ec66b422f2eff4386
Wannes Meert <wannes.meert@cs.kuleuven.be>
2012-03-06 01:12:47 +0100

    distribution.sh: update

--- distribute.sh
+++ distribute.sh
@@ -4,18 +4,19 @@ git describe --tags --long > VERSION
 git show -s --format="%H %ci" >> VERSION
 
 echo VERSION Makefile Makefile.settings compile.py compile_functions.py README.txt *.cls *.bib chapters *.tex *.py *.sty image >MANIFEST2;
-mv MANIFEST2 MANIFEST
-cat MANIFEST | tr ' ' '\n' | sed -e "s/^/adsphd.src\//g" > MANIFEST; 
+cat MANIFEST2
+cat MANIFEST2 | tr ' ' '\n' | sed -e "s/^/adsphd.src\//g" > MANIFEST; 
 
 ADSPHDDIR=$(pwd)
 cd ..
 
-if [ ! -a $ADSPHDDIR/adsphd.src ]; then
-	ln -s $ADSPHDDIR adsphd.src;
+if [ ! -h adsphd.src ]; then
+	ln -s $ADSPHDDIR adsphd.src
 fi;
 
-echo $ADSPHDDIR/MANIFEST;
-
+echo "TARRING"
 tar -czvf $ADSPHDDIR/adsphd.src.tgz $(cat $ADSPHDDIR/MANIFEST);
-rm $ADSPHDDIR/MANIFEST; rm adsphd.src;
+rm $ADSPHDDIR/MANIFEST;
+rm adsphd.src;
+
 
